6fd5a1d5ddd5d564de9ad2761bed8ecd38f7c1bc
TheBrellis <brianroyalellis@gmail.com>
2019-12-19 18:39:15 -0500

    added comment to server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // by requiring the whole routes folder, we are able to de-couple the server.js file from the routing, allowing us to make modifications to our routing structure without having to modify our server.js file.  When requiring an entire directory, you must put an index.js file in the root of that directory to instruct express how to direct requests further up the routing tree.
-const routes = require('./routes')
+const routes = require('./routes')// when requiring a folder using app.get(), must have an index.js in that folder.
 
 // once the route directory has been required, app.use takes two arguments, the path fragment and the directory that will be used after hitting that portion of the route.  In this case, if the user hits the base url, express will then look to the index file in the routes directory to direct traffic beyond that base url.
 app.use('/', routes)
